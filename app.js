9c19e478710d68851b764d47428c7f13129d47b6
jyo1706 <jsharma680@yahoo.com>
2024-02-05 00:22:06 +0530

    add cors

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const connectDb = require('./Db/connectDb')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const flash = require('connect-flash')
-
+const cors = require('cors')
 app.use(fileUpload({useTempFiles:true}))
 // app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(express.json());
 // app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
+app.use(cors())
 app.use(flash())
 connectDb()
 app.use('/api',web)
