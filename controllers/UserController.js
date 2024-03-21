63261055d7719f8f904801dabeb9814a488bab47
jyo1706 <93206041+jyo1706@users.noreply.github.com>
2024-03-22 04:49:32 +0530

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -134,6 +134,96 @@ class UserController {
         console.log(error)
       }
     }
+  static logout = async (req, res) => {
+    try {
+      res.clearCookie("token");
+      res
+        .status(200)
+        .json({ status: "Success", message: "Logout Successfully" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+   static change_password = async (req, res) => {
+    try {
+      const { name, email, id, image } = req.data;
+      // console.log(req.body)
+      const { oldpassword, newpassword, cpassword } = req.body;
+      if (oldpassword && newpassword && cpassword) {
+        const user = await userModel.findById(id);
+        const ismatch = await bcrypt.compare(oldpassword, user.password);
+        if (!ismatch) {
+          res
+            .status(401)
+            .json({ status: "Failed", message: "Old Password is incorrect" });
+        } else {
+          if (newpassword !== cpassword) {
+            res
+              .status(401)
+              .json({
+                status: "Failed",
+                message: "Password and confirm password is not matched",
+              });
+          } else {
+            const newHashpassword = await bcrypt.hash(newpassword, 10);
+            await userModel.findByIdAndUpdate(id, {
+              $set: { password: newHashpassword },
+            });
+
+            res
+              .status(201)
+              .json({
+                status: "Success",
+                message: "Password Change successfully",
+              });
+          }
+        }
+      } else {
+        res
+          .status(400)
+          .json({ status: "Failed", message: "All field are required" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static profile_update = async (req, res) => {
+    try {
+      const {id,email,name}=req.data
+      //console.log(req.files.image)
+      if (req.files) {
+        const user = await userModel.findById(req.data.id);
+        const image_id = user.image.public_id;
+        await cloudinary.uploader.destroy(image_id);
+
+        const file = req.files.image;
+        const myimage = await cloudinary.uploader.upload(file.tempFilePath, {
+          folder: "emcommerce",
+        });
+        var data = {
+          name: req.body.name,
+          email: req.body.email,
+          image: {
+            public_id: myimage.public_id,
+            url: myimage.secure_url,
+          },
+        };
+      } else {
+        var data = {
+          name: req.body.name,
+          email: req.body.email,
+        };
+      }
+      const update_profile = await UserModel.findByIdAndUpdate(
+        req.data.id,
+        data
+      );
+      res.redirect("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 module.exports = UserController;
